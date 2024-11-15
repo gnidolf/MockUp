18f52ea27c3a1cd1e50490ef8e0c3ed615fbbd26
Me <you@example.com>
2024-11-15 15:12:15 +0300

    postValue

--- src/main/java/com/example/MockUp/Controller/MainController.java
+++ src/main/java/com/example/MockUp/Controller/MainController.java
@@ -2,6 +2,8 @@ package com.example.MockUp.Controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory; 
 
@@ -12,4 +14,18 @@ public class MainController {
 
     ObjectMapper mapper = new ObjectMapper();
 
+    @PostMapping(
+        value = "/test",
+        producers = MediaType.APPLICATION_JSON_VALUE,
+        consumers = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object postValue() {
+
+        try {
+            String id = RequestDTO.getid();
+            char firstDigit.charAt()
+        } catch {
+
+        }
+    }
 }
