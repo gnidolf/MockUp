7cb2b81198025aeac7b142ec6c7a6c2b5353b44d
Me <you@example.com>
2024-11-18 13:59:23 +0300

    finel commit

--- src/main/java/com/example/MockUp/Controller/MainController.java
+++ src/main/java/com/example/MockUp/Controller/MainController.java
@@ -1,17 +1,17 @@
 package com.example.MockUp.Controller;
 
-import com.example.MockUp.Model.*;
 import java.util.ArrayList;
 import java.util.List;
+import com.example.MockUp.Model.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
-// import org.slf4j.*; доделать логирование
+import org.slf4j.*;
 
 @RestController
 public class MainController {
 
-    // private Logger log = LoggerFactory.getLogger(MainController.class);
+    private Logger log = LoggerFactory.getLogger(MainController.class);
 
     ObjectMapper mapper = new ObjectMapper();
 
@@ -20,19 +20,30 @@ public class MainController {
         produces = MediaType.APPLICATION_JSON_VALUE,
         consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    public Object postValue(@RequestBody Request requestDTO) {
+    public Object postValue(@RequestBody Request request) {
+
+        log.info("Получен запрос: {}", request);
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            log.error("Ошибка при ожидании: {}", e.getMessage());
+            Thread.currentThread().interrupt();
+        }
         
         List<Response> responseList = new ArrayList<Response>();
 
-        int count = requestDTO.getCount();
+        int count = request.getCount();
         
         for(int i = 1; i < count+1; i++){
-            Response responseDTO = new Response();
-            responseDTO.setLastname("lastname" + i);
-            responseDTO.setFirstname("firstname" + i);
-            responseDTO.setId(i);
-            responseList.add(responseDTO);
+            Response response = new Response();
+            response.setLastname("lastname" + i);
+            response.setFirstname("firstname" + i);
+            response.setId(i);
+            responseList.add(response);
         }
+
+        log.info("Отправлен ответ: {}", responseList);
         
         return responseList;
 
